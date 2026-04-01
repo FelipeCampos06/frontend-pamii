5094a0363af3f02340790fc81412d0898ac13c86
FelipeCampos06 <felipe.belinati@gmail.com>
2026-04-01 20:05:31 -0300

    aula 01/04

--- src/pages/produto/CadProdutoPage.js
+++ src/pages/produto/CadProdutoPage.js
@@ -9,7 +9,30 @@ class CadProdutoPage extends HTMLElement {
         this.classList.add('ion-page');
         const cabecalho = createHeader(pageName);
         this.innerHTML = `
-            ${cabecalho}
+            ${cabecalho} 
+
+        <ion-content class = "ion-padding">
+            <from id = "from=produto">
+                <ion-list>
+                    <ion-item>
+                        <ion-input type = "text" name = "dsc_produto" label = "Descrição do produto" label-placement = "floating" required>
+                        </ion-input> 
+                    </ion-item>
+
+                    <ion-item>
+                        <ion-input type = "number" step = "0.01" name = "valor_unit" 
+                        label = "Valor Unitário" label-placement  = "floating" required>
+                        </ion-input> 
+                    </ion-item>
+
+                    <ion-item>
+                        <ion-label> Ativo </ion-label>
+                        <ion-toggle slot = "end" name = "status" checked></ion-toggle> 
+                    </ion-item>
+                </ion-list>  
+            </from>
+        </ion-content>
+
         `;
 
         this.querySelector('#logout-btn').addEventListener('click', logout);
